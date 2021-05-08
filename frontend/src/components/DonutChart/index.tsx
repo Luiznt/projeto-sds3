7e624b05be61074c4a3ceca1ec353094a25afe13
Luiznt <luiznt@hotmail.com>
2021-05-08 17:52:38 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,20 +1,61 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SalesSum } from 'types/sales';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
+
 const DonutChart = () => {
+
+    // Forma errada. exibida aqui apenas para efeitos didaticos.
+    let chartData: ChartData = { labels: [], series: [] };
+    // a string do get pode ser com crase: `${BASE_URL}/sales/amount-by-seller`
+    // ou com aspas simples: BASE_URL + '/sales/amount-by-seller'
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then(response => {
+            const data = response.data as SalesSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            chartData = {labels: myLabels, series: mySeries};
+            //console.log(response.data)
+            console.log(chartData);
+        }
+        );
+    // a forma errada faz com que ocorra esta chamada varias vezes pelo browser.
+
+    /* usado apenas como mockup
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
-    
+    */
+
     const options = {
         legend: {
             show: true
         }
     }
 
+    /* usado no mockup
     return (
         <Chart
             options={{ ...options, labels: mockData.labels }}
             series={mockData.series}
+            type="donut"
+            height="240"
+
+
+        />
+    );
+    */
+    return (
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240"
 
