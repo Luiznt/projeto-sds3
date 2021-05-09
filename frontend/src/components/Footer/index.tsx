96d2c7636336a9cb719b096f7c259e193074faa1
Luiznt <luiznt@hotmail.com>
2021-05-08 23:50:44 -0300

    Footer adjust

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,11 +2,12 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/Luiznt" target="_blank" rel="noreferrer">Luiz Coelho</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/luiz-coelho-492990203/" target="_blank" rel="noreferrer">Luiz Coelho</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a><br />
+            Github da aplicação <a href="https://github.com/Luiznt/projeto-sds3" target="_blank" rel="noreferrer">DSVendas</a></small></p>
             </div>
-        </footer>
+        </footer >
     );
 }
 
